70801aa11d64afaa682b97ed2150f3cf8ab87033
ling thang <112737273+LingThang@users.noreply.github.com>
2022-10-15 23:58:34 +0000

    added option choices/user inputs for the questions asked: Whats the problem, and if recording was allowed. Planning on next adding series of questions related to the delivery

--- main.py
+++ main.py
@@ -2,20 +2,31 @@
 import random
 
 name = input("What is your name? ")
-print("Hi there, " + name  + ". My name is 2B, your AI best friend! Nice to see you again!")
 
-#asks how their day was
+print("Hi there, " + name  + ". My name is 2B, your AI helper. How may I assist you today?")
 
-#happy response= asking why, and if anything good happened
+optionchoice = input("A: Nevermind B: My order hasn't arrived.")
 
-#sad response= asking why, if bot could help 
+continB = "b"
 
-# - yes, ask how, show options (music, phone number based on major issue [police, therapist ect.])
+continA = 1
 
-#- NO, Reaffirms User, switches topic
+print()
 
-#Topics - Hobbies, weather, fav:(color, food, store, etc)
+def pconvo():
+ optionchoice = input("1: Nevermind 2: My order hasn't arrived.")
+if optionchoice.upper == continA.upper:
+  print("I see, i'll be here to assist you whenever you need, have a nice day.")
+if optionchoice.upper == continB.upper:
+  storeinfo = input("To insure, and improve the chat box, and future interactions between clients, will you allow the recording of this conversation. Y/N")
 
-#continuous expanding of topic - sub topics and questions untill User quits
 
-#adding this code to test out and confirm pull request from github
+infoPermiY = 'y'
+
+infoPermiN = 'n'
+
+if storeinfo.upper == infoPermiN.upper:
+  print("I see.")
+  
+if storeinfo.upper == infoPermiY.upper:
+  print("Thank you for your participation.")
