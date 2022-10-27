9e8a0ed760a0f2d38fe5afb29a6a00f433e23ee8
ling thang <112737273+LingThang@users.noreply.github.com>
2022-10-27 22:27:41 +0000

    coded the final response,

--- main.py
+++ main.py
@@ -1,46 +1,52 @@
 
 import random
 
-name = input("What is your name? ")
+name = input("What is your name?  ")
 
 print("Hi there, " + name  + ". My name is 2B, your AI helper. How may I assist you today?")
 
-optionchoice = input("A: Nevermind B: My order hasn't arrived.")
+optionchoice = input("A: Nevermind B: My order hasn't arrived.  ")
 
 continB = "b"
 
-continA = 1
+continA = "A"
 
 print()
 
 def pconvo():
- optionchoice = input("1: Nevermind 2: My order hasn't arrived.")
+ optionchoice = input("1: Nevermind 2: My order hasn't arrived. ")
 if optionchoice.upper == continA.upper:
   print("I see, i'll be here to assist you whenever you need, have a nice day.")
 if optionchoice.upper == continB.upper:
-  storeinfo = input("To insure, and improve the chat box, and future interactions between clients, will you allow the recording of this conversation. Y/N")
+  storeinfo = input("To insure, and improve the chat box, and future interactions between clients, will you allow the recording of this conversation. Y/N  ")
 
-
-infoPermiY = 'y'
-
-infoPermiN = 'n'
-
-if storeinfo.upper == infoPermiN.upper:
-  print("I see.")
-  
-if storeinfo.upper == infoPermiY.upper:
   print("Thank you for your participation.")
 
-userID = input("What is your User Id.  " )
-print()
+  userID = input("What is your User Id.  " )
+  print()
 
-ConfirmUserId = input(f' {userID}  Y/N ?')
+  ConfirmUserId = input(f' {userID}  Y/N?  ')
 
-deliverID = input("What was the dilivery ID?")
+  print("please wait a moment....")
 
-print("Id found, please wait a moment as we confirm your delivery status.  ")
+  print("Id found, please wait a moment as we confirm your delivery status.  ")
 
-print('.')
-print('.')
-print('.')
-print('.')
+  print('.')
+  print('.')
+  print('.')
+  print('.')
+
+  print("Delivery status found.. ")
+  print("Delivery is on the way.. ")
+  print("Showing status..")
+
+  print(f"Id|{userID}\nStatus|In Transport\n Estimated arrival| 2H 28M")
+  
+  print()
+  print()
+  
+  print("I hope you were satisfied with your service, if you had any inconveniences please report the issue. Have a good day ^-^!")
+
+
+
+
